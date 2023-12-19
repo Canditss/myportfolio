460e22167e6b26636a5c63591da2d3809c242277
Radinka <radindanahara@gmail.com>
2023-12-19 14:02:55 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleAboutButton() {
+  var aboutElement = document.querySelector('.about');
+  aboutElement.style.display = (aboutElement.style.display === 'none' || aboutElement.style.display === '') ? 'block' : 'none';
+}
+
+
