362ceb35d19fa39160593ea1336c493cc3011e7e
Canditss <135102176+Canditss@users.noreply.github.com>
2023-12-21 00:35:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function toggleAboutButton() {
   var aboutElement = document.querySelector('.about');
-  aboutElement.style.display = (aboutElement.style.display === 'none' || aboutElement.style.display === '') ? 'block' : 'none';
+  aboutElement.style.display = (aboutElement.style.display === 'none' || aboutElement.style.display === '') ? 'block' : '';
   var button = document.getElementById('toggleAboutButton');
   button.textContent = (button.textContent === '▲') ? 'Details ▼' : '▲';
 }
