2c546571fa4b5d0c827a4dd3ca4f453cc261d23d
Canditss <135102176+Canditss@users.noreply.github.com>
2024-05-13 05:01:15 +0700

    Update scrollreveal.js

--- scrollreveal.js
+++ scrollreveal.js
@@ -1,4 +1,8 @@
 ScrollReveal().reveal('.profile-card', { delay: 100, reset: true, desktop: false});
 ScrollReveal().reveal('.profile-pic', { delay: 350, reset: true, desktop: false});
 ScrollReveal().reveal('.profile-details', { delay: 550, reset: true, desktop: false});
-
+ScrollReveal().reveal('.fa', { duration: 3550, reset: true, scale: 3, desktop: false});
+ScrollReveal().reveal('.fa-facebook', {delay: 650, duration: 1500, reset: true, scale: 3, desktop: false});
+ScrollReveal().reveal('.fa-github', {delay: 750, duration: 2000, reset: true, scale: 3, desktop: false});
+ScrollReveal().reveal('.fa-tiktok', {delay: 850, duration: 2500, reset: true, scale: 3, desktop: false});
+ScrollReveal().reveal('.fa-instagram', {delay: 950, duration: 3100, reset: true, scale: 3, desktop: false});
