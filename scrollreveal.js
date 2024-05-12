aef51d672fb160f1d759642a68adaef85ac8af8d
Canditss <135102176+Canditss@users.noreply.github.com>
2024-05-13 04:20:42 +0700

    Add files via upload

---
+++ scrollreveal.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.profile-card', { delay: 100, reset: true, desktop: false});
+ScrollReveal().reveal('.profile-pic', { delay: 350, reset: true, desktop: false});
+ScrollReveal().reveal('.profile-details', { delay: 550, reset: true, desktop: false});
+
